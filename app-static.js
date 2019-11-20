91de6aebbd826e2fa77819cf4c805fbe100af83d
Tim Ngo <ngotm.tn@gmail.com>
2019-11-19 21:16:47 -0800

    mobile hover fix

--- app-static.js
+++
@@ -1,8 +0,0 @@
-const Express = require('express');
-const app = Express();
-// const server = require('http').createServer(app);
-
-
-app.use(Express.static(__dirname + "\\..\\client\\dist"))
-
-app.listen(process.env.PORT || 8000);
--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const WebSocket = require('ws');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 app
